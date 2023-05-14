10231b7577db52f3b136b3e4442a619f43a3b908
samson-fcb <105276013+samson-fcb@users.noreply.github.com>
2023-05-14 14:59:38 +0000

    update

--- src/components/canvas/Ball.tsx
+++ src/components/canvas/Ball.tsx
@@ -14,7 +14,12 @@ const Ball = (props: any) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+    // @ts-ignore
+    <Float
+      speed={1.75}
+      rotationIntensity={1}
+      floatIntensity={2}
+    >
       <ambientLight intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
